49ca61189cb96f4c2a6aa0c5ac22ba8f8cb21b08
Caio Ramos <caio.ramos@tabmedia.com.br>
2018-12-15 23:37:13 -0200

    Modifications in the UserRepository

--- Domain/Users/Repositories/UserRepository.cs
+++ Domain/Users/Repositories/UserRepository.cs
@@ -5,8 +5,6 @@ namespace WorkDayLog.Domain.Users.Repositories
 {
     public class UserRepository : RepositoryBase<User>, IUserRepository
     {
-        private readonly string _connectionString;
-
         protected UserRepository(IConfiguration config) 
             : base(config, "Users") {}
     }
